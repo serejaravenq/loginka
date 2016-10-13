e68349689c5f8ece8bdd51e740a644d73d6a0dc9
Sergeev Dmitry <sergeevdms@gmail.com>
2016-10-14 02:48:35 +0300

    change logic

--- login.php
+++ login.php
@@ -1,21 +1,34 @@
 <?php
-$user = $_POST["username"];
+// show all errors on
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(-1);
+
+// input POST params
+$email = $_POST["email"];
 $pass = $_POST["password"];
-//var_dump($user);
+
+// mysql connection
 $db = new mysqli("localhost", "root", "");
-$db->select_db("wapeshop");
-$r = $db->query("SELECT * FROM admins ");
-//var_dump($r);
-$db->close();
- $row = $r->fetch_assoc() ;
-//var_dump($row);
-//echo $row['email'];
-if(($row['email'] == $user) && ($row['Password'] == $pass)){
-	echo "Вы авторизовались";
-	 setcookie('username', $user, time()+3600);
-}else{
-	echo "не правильно введены данные";
+$db->select_db("vapeshop");
+
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
 }
 
+$result = $db->query("SELECT * FROM admins");
 
+$db->close();
+
+$row = $result->fetch_assoc();
+
+if (($row['email'] == $email) && ($row['password'] == $pass)) {
+	echo "Вы авторизовались \n";
+	var_dump($row);
+	 //setcookie('username', $user, time()+3600);
+} else {
+	echo "не правильно введены данные";
+}
 ?>
